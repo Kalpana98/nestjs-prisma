0fdfd7cfdf506031b4dbf730f99d6bdc679e3635
Kalpana98 <kalpana360395@gmail.com>
2022-04-14 22:30:43 +0530

    First Commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe(@GetUser() user: User, @GetUser('email') email: string) {
+    console.log(email);
+    return user;
+  }
+
+  // @Patch()
+  // editUser() {}
+}
